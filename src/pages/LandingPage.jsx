2261e9b9a555bcd407a6517f4d1ce410370bb67c
beatrice <beatrice.pepple@gmail.com>
2024-06-14 20:52:15 +0100

    landing mobile view similar to design now

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; // Importing useState hook
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Footer } from './components';
 import logo from '../assets/icons/chatCircleLogo.svg';
@@ -10,11 +10,25 @@ import heroImage from '../assets/images/hero_img1.png';
 import heroGroup from '../assets/images/hero_group1.png';
 import popularImage from '../assets/images/popular_img.png';
 import engagingImage from '../assets/images/engagingchats.png';
+import heroMiniImage from '../assets/images/hero_mini.png';
+import popularMini from '../assets/images/mini_popular.png';
+import engagingMini from '../assets/images/mini_engaging.png';
+import { FiMenu, FiX } from 'react-icons/fi';
 
 const LandingPage = () => {
   const [mode, setMode] = useState('signup'); // State to track current mode: 'signin' or 'signup'
   const [startDiscussion, setStartDiscussion] = useState(false); // State to track Start a discussion toggle
   const [browseTopics, setBrowseTopics] = useState(false); // State to track Browse topics toggle
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
 
   const toggleMode = () => {
     setMode(prevMode => prevMode === 'signup' ? 'signin' : 'signup');
@@ -32,25 +46,36 @@ const LandingPage = () => {
     <div className='container max-w-screen-xl m-auto flex flex-col gap-10 px-4 md:p-0'>
       <nav className="justify-between m-0 mt-4">
         <div className="container mx-auto flex justify-between items-center flex-col md:flex-row gap-6">
-          <Link to="/" className="text-white text-xl font-bold"><img src={logo} alt="chat circle logo" /></Link>
-            <div className="relative w-full max-w-lg">
-              <div className="absolute left-2 top-2 text-white">
-                <img src={searchIcon} alt="search Icon" />
-              </div>
-              <input
-                type="text"
-                placeholder="Search for a topic..."
-                className="py-2 pl-10 pr-4 rounded rounded-xl  text-textGrey placeholder-gray-400 focus:outline-none w-full border border-lightGrey"
-              />
-              <button className="absolute right-2 top-2 text-white">
-              <img src={microphoneIcon} alt="search Icon" />
-              </button>
+          <div className="flex justify-between items-center w-full md:w-auto">
+            <button onClick={toggleMenu} className="text-gray-600 focus:outline-none md:hidden">
+              {isMenuOpen ? <FiX size={24} /> : <FiMenu size={24} />}
+            </button>
+            <Link to="/" className="text-white text-xl font-bold"><img src={logo} alt="chat circle logo" /></Link>
+            <button onClick={toggleSearch} className="text-white focus:outline-none md:hidden">
+              <img src={searchIcon} alt="search Icon" />
+            </button>
+          </div>
+          <div className={`relative w-full max-w-lg ${isSearchOpen ? 'block' : 'hidden'} md:block`}>
+            <div className="absolute left-2 top-2 text-white">
+              <img src={searchIcon} alt="search Icon" />
             </div>
-            <div className="flex flex-col sm:flex-row gap-6 w-4/5 sm:w-3/10 lg:w-1/3 justify-between mx-auto lg:mx-0">
-              <Link to="/SignUp" className="text-white hover:text-white border bg-primaryOrange border-primaryOrange px-4 py-2 rounded rounded-xl w-full lg:w-48 md:w-36 text-center font-bold mb-4 sm:mb-0">SIGN UP</Link>
-              <Link to="/Login" className="text-primaryOrange hover:text-white border border-primaryOrange px-4 py-2 rounded rounded-xl w-full lg:w-48 md:w-36 text-center font-bold">SIGN IN</Link>
-            </div>
-
+            <input
+              type="text"
+              placeholder="Search for a topic..."
+              className="py-2 pl-10 pr-4 rounded-xl text-textGrey placeholder-gray-400 focus:outline-none w-full border border-lightGrey"
+            />
+            <button className="absolute right-2 top-2 text-white">
+              <img src={microphoneIcon} alt="microphone Icon" />
+            </button>
+          </div>
+          <div className="hidden md:flex flex-col sm:flex-row gap-6 w-4/5 sm:w-3/10 lg:w-1/3 justify-between mx-auto lg:mx-0">
+            <Link to="/SignUp" className="text-white hover:text-white border bg-primaryOrange border-primaryOrange px-4 py-2 rounded-xl w-full lg:w-48 md:w-36 text-center font-bold mb-4 sm:mb-0">SIGN UP</Link>
+            <Link to="/Login" className="text-primaryOrange hover:text-white border border-primaryOrange px-4 py-2 rounded-xl w-full lg:w-48 md:w-36 text-center font-bold">SIGN IN</Link>
+          </div>
+        </div>
+        <div className={`md:hidden flex-col items-center ${isMenuOpen ? 'flex' : 'hidden'}`}>
+          <Link to="/SignUp" className="text-white hover:text-white border bg-primaryOrange border-primaryOrange px-4 py-2 rounded-xl w-full text-center font-bold mb-4">SIGN UP</Link>
+          <Link to="/Login" className="text-primaryOrange hover:text-white border border-primaryOrange px-4 py-2 rounded-xl w-full text-center font-bold">SIGN IN</Link>
         </div>
       </nav>
       <div className="flex pt-10 hero-background justify-between items-center border border-grey-200 border-l-0 border-r-0">
@@ -58,7 +83,10 @@ const LandingPage = () => {
     <h2 className="text-3xl md:text-4xl lg:text-5xl text-textGrey font-normal leading-tight">
       Post <span className='text-primaryOrange font-bold'>questions</span>, Get <span className='text-primaryOrange font-bold'>answers</span>, and Engage in <span className='text-primaryOrange font-bold'>discussions</span> on various topics
     </h2>
-    <div className="flex mt-4 gap-1">
+    <div className="flex flex-col md:flex-row mt-4 gap-2 ">
+      <div className=' md:hidden w-full'>
+        <img src={heroMiniImage} alt="mini version of the hero image w-full" />
+      </div>
       <button className={`button-action button-base ${startDiscussion ? 'bg-primaryOrange text-white' : 'bg-white text-gray-700'}`} onClick={handleStartDiscussion}>START A DISCUSSION</button>
       <button className={`button-base text-primaryOrange gap-3 ${browseTopics ? 'bg-primaryOrange text-white' : 'bg-white text-gray-700'}`} onClick={handleBrowseTopics}>BROWSE TOPICS<img src={arrowIcon} alt="Arrow Icon" /></button>
     </div>
@@ -72,25 +100,31 @@ const LandingPage = () => {
 {/* popular */}
 
       <div className="mt-12 flex flex-col md:flex-row gap-4 max-h-max border border-grey-200 border-l-0 border-r-0">
-          <div className="w-full md:w-2/4">
+          <div className="w-full md:w-2/4 hidden md:flex">
           <img src={popularImage} alt="Screenshots of popular threads"/>
           </div>
-          <div className="w-full md:w-2/4 flex flex-col justify-center gap-8">
-          <h2 className="text-3xl md:text-2xl font-bold text-primaryOrange"><span className='underlines'>Popular O</span>N ChatCircle</h2>
-          <p className='text-3xl md:text-5xl font-normal leading-tight text-textGrey'>Find answers, share knowledge, and connect with passionate people</p>
+          <div className="w-full md:w-2/4 flex flex-col justify-center gap-6">
+          <h2 className="text-2xl md:text-2xl font-bold text-primaryOrange"><span className='underlines'>Popular O</span>N ChatCircle</h2>
+          <p className='text-xl md:text-5xl font-normal leading-tight text-textGrey'>Find answers, share knowledge, and connect with passionate people</p>
           </div>
+          <div className=' md:hidden w-full'>
+        <img src={popularMini} alt="mini version of the popular threads image w-full" />
+      </div>
       </div>
 
       {/* engaging chats */}
 
-      <div className="mt-12 flex flex-col-reverse md:flex-row items-center border border-grey-200 border-l-0 border-r-0">
-  <div className="w-full md:w-2/4 flex flex-col gap-8">
+      <div className="mt-12 flex flex-col md:flex-row items-center border border-grey-200 border-l-0 border-r-0">
+  <div className="w-full md:w-2/4 flex flex-col gap-6">
     <h2 className="text-2xl md:text-4xl font-bold w-3/4 text-primaryOrange">Most Engaging threads on <span className='underlines'>ChatCircle</span></h2>
-    <p className='text-2xl md:text-5xl font-normal leading-tight text-textGrey tracking-wider'>Have a closer look at trending topics and discussions in our forum</p>
+    <p className='text-xl md:text-5xl font-normal leading-tight text-textGrey tracking-wider'>Have a closer look at trending topics and discussions in our forum</p>
   </div>
-  <div className="w-full md:w-2/4 md:pl-8">
+  <div className="w-full md:w-2/4 md:pl-8 hidden md:flex">
     <img src={engagingImage} alt="Screenshots of engaging chats" className="w-full md:w-auto max-w-full h-auto md:h-auto" />
   </div>
+  <div className=' md:hidden w-full mt-4'>
+        <img src={engagingMini} alt="mini version of the hero image w-full" />
+      </div>
 </div>
 
 
