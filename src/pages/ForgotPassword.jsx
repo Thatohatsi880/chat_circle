a2833fb46b0b64909b8a7010b3650f5886ff2aff
beatrice <beatrice.pepple@gmail.com>
2024-06-14 22:33:53 +0100

    done with the forgot password

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react';
-import { FiEye, FiEyeOff } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import logo from '../assets/icons/chatCircleLogo.svg';
-import googleIcon from '../assets/icons/flat-color-icons_google.svg';
-import signupImage from '../assets/images/signup_img.png';
+import forgotPwdImg from '../assets/icons/fgtPwdIllustration.svg'
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState('');
@@ -19,28 +17,51 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-      <div className="bg-white p-6 rounded shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-4 text-center">Forgot Password</h2>
-        <form onSubmit={handleResetPassword} className="flex flex-col gap-4">
+    <div className="min-h-screen flex w-full border border-pink-800">
+        {/* Image */}
+         <div className="hidden lg:flex md:w-[45%] bg-bgOrange items-center justify-center">
+            <div className="w-4/5 m-auto">
+              <img src={forgotPwdImg} alt="" className='z-4 w-full h-auto' />
+            </div>
+          </div>
+
+{/* Form */}
+      <div className='w-full md:w-[55%] flex justify-center items-center p-4'>
+        <div className='p-6 rounded shadow-md w-full max-w-md flex flex-col gap-3'>
+            <div className="flex justify-center mb-4">
+                <Link to="/" className="text-white text-xl font-bold"><img src={logo} alt="chat circle logo" /></Link>
+            </div>
+                <h2 className='text-center text-gray-600 mb-4 font-bold'>Password Reset</h2>
+                {/* Text below logo */}
+                <p className="text-center text-gray-600 text-sm">
+                Please enter the following details to reset your password. Password will be sent to your email.
+                </p>
+            <br />
+          
+            <form onSubmit={handleResetPassword} className="flex flex-col gap-4 my-6">
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email Address</label>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email:</label>
             <input
               type="email"
               id="email"
               value={email}
               onChange={handleEmailChange}
               required
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primaryOrange focus:border-transparent"
+              placeholder='Enter email here'
+              className="mt-1 p-2 w-full rounded-xl h-12 text-xs border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primaryOrange focus:border-transparent"
             />
           </div>
           <button
             type="submit"
-            className="bg-primaryOrange text-white font-bold py-2 px-4 rounded-md hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-opacity-50"
+            className="bg-primaryOrange text-white font-bold mt-6 py-2 px-4 rounded-xl hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-opacity-50"
           >
-            Reset Password
+            Reset 
           </button>
         </form>
+
+        <p className="text-center text-textGrey mt-4">Already have an account? <Link to="/Login" className="text-textGrey font-bold underline">Login</Link></p>
+        </div>
+      
       </div>
     </div>
   );
